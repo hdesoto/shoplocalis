f413e0718e6a6c438dd44dd2ba6e40372d20426d
Hernan de Soto <hdesoto@gmail.com>
2017-08-22 17:10:08 +0200

    Home wireframing in progress

---
+++ server/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+
+
+const app = express()
+
+
+const PORT = process.env.PORT || 3001
+
+app.use(express.static(path.join(__dirname, '../client' )))
+
+
+
+
+
+
+
+app.listen(PORT)
+console.log(`Listening on PORT ${PORT}`)
