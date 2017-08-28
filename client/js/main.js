c4d52a778bd730004026fe89b68738b91afb5018
Hernan de Soto <hdesoto@gmail.com>
2017-08-28 16:19:47 +0200

    Folders rearranged, templates corrected, added checkout template

--- client/js/main.js
+++ client/js/main.js
@@ -1,82 +1,24 @@
 /* $ global */
 
-$('.menu-toggle').click(function(){
-  $('.site-nav').toggleClass('site-nav--open');
-  $(this).toggleClass('menu-toggle--open');
+$('.btn-add-to-cart').click(function(){
+  $('.alert-added-to-cart').toggleClass('hidden');
+})
+
+$('.btn-close-alert').click(function(){
+  $('.alert-added-to-cart').toggleClass('hidden');
 })
 
 
-$('.search-launch').click(function(){
-  toggleSearch();
-  $('.top-input').focus()
+/* ==== PRODUCT DETAILS ==== */
+
+$('.btn-plus').click( function() {
+  var currentQuantity = $('.quantity-required').val()
+  var newQuantity = +currentQuantity + 1
+  $('.quantity-required').val(newQuantity.toString())
 })
 
-
-
-
-$('.top-input').on("blur", function(){
-  toggleSearch();
+$('.btn-minus').click( function() {
+  var currentQuantity = $('.quantity-required').val()
+  var newQuantity = +currentQuantity - 1
+  $('.quantity-required').val(newQuantity.toString())
 })
-
-
-$('.user').click(function(){
-  $('.login-pane').toggleClass('hidden')
-})
-
-
-
-function toggleSearch(){
-  $('.top-input').toggleClass('show-search');
-  $('.site-nav').toggleClass('search--open');
-  $('.user-icon').toggleClass('search--open');
-  $('.search-icon').toggleClass('search--open');
-  $('.cart-icon').toggleClass('search--open');
-  $('.menu-toggle').toggleClass('search--open');
-}
-
-
-
-
-$('.cart-icon').click(function(){
-  $('.site-nav-carrito').toggleClass('hidden')
-})
-
-$('.open-shopping-cart').click(function(){
-  $('.site-nav-carrito').toggleClass('hidden')
-})
-
-
-// $('.like-btn').on('click', function() {
-//    $(this).toggleClass('is-active');
-// });
-
-$('.minus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value > 1) {
-        value = value - 1;
-    } else {
-        value = 0;
-    }
- 
-  $input.val(value);
- 
-});
- 
-$('.plus-btn').on('click', function(e) {
-    e.preventDefault();
-    var $this = $(this);
-    var $input = $this.closest('div').find('input');
-    var value = parseInt($input.val());
- 
-    if (value < 100) {
-        value = value + 1;
-    } else {
-        value =100;
-    }
- 
-    $input.val(value);
-});
--- client/js/product-details.js
+++ client/js/product-details.js
@@ -1,33 +1,13 @@
-   $(document).ready(function() {
-       //-- Click on detail
-       $("ul.menu-items > li").on("click", function() {
-           $("ul.menu-items > li").removeClass("active");
-           $(this).addClass("active");
-       })
+// $ global //
 
-       $(".attr,.attr2").on("click", function() {
-           var clase = $(this).attr("class");
+$('.btn-plus').click( function() {
+  var currentQuantity = $('.quantity-required').val()
+  var newQuantity = +currentQuantity + 1
+  $('.quantity-required').val(newQuantity.toString())
+})
 
-           $("." + clase).removeClass("active");
-           $(this).addClass("active");
-       })
-
-       //-- Click on QUANTITY
-       $(".btn-minus").on("click", function() {
-           var now = $(".section > div > input").val();
-           if ($.isNumeric(now)) {
-               if (parseInt(now) - 1 > 0) { now--; }
-               $(".section > div > input").val(now);
-           } else {
-               $(".section > div > input").val("1");
-           }
-       })
-       $(".btn-plus").on("click", function() {
-           var now = $(".section > div > input").val();
-           if ($.isNumeric(now)) {
-               $(".section > div > input").val(parseInt(now) + 1);
-           } else {
-               $(".section > div > input").val("1");
-           }
-       })
-   })
+$('.btn-minus').click( function() {
+  var currentQuantity = $('.quantity-required').val()
+  var newQuantity = +currentQuantity - 1
+  $('.quantity-required').val(newQuantity.toString())
+})
